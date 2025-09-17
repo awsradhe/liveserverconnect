67baa9dc4674e017aecc12766e0442a51a2671da
Radhe Shyam <softwaredevelopment311@gmail.com>
2025-09-18 00:52:42 +0530

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+		<h1>Welcome to Laravel on AWS Cloud</h1>
+		<a href="{{route('nextpages')}}">Go Next Page</a>
+</body>
+</html>
